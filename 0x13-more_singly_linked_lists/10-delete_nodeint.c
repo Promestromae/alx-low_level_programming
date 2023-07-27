8837d25bef0c175b5e8eaa7db77a39d1ad2bdb39
Promestromae <nzuvameshack@gmail.com>
2023-07-27 14:32:54 -0700

    lovend

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,27 +8,26 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-    listint_t *temp = *head;
-    listint_t *current = NULL;
-    unsigned int i = 0;
+    listint_t *temp, *current;
+    unsigned int i;
 
     if (*head == NULL)
         return (-1);
 
     if (index == 0)
     {
+        current = *head;
         *head = (*head)->next;
-        free(temp);
+        free(current);
         return (1);
     }
 
-    while (i < index - 1)
-    {
-        if (!temp || !(temp->next))
-            return (-1);
+    temp = *head;
+    for (i = 0; temp != NULL && i < index - 1; i++)
         temp = temp->next;
-        i++;
-    }
+
+    if (temp == NULL || temp->next == NULL)
+        return (-1);
 
     current = temp->next;
     temp->next = current->next;

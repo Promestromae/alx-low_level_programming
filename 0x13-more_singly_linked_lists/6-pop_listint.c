1384e4ae2d2e2d53254ad8effb99a3e003cba7bd
Promestromae <nzuvameshack@gmail.com>
2023-07-27 15:08:50 -0700

    wise

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,4 +1,4 @@
-include "lists.h"
+#include "lists.h"
 
 /**
  * pop_listint - function that deletes the head node of a listint_t linked list, and returns the head node’s data (n).
--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,4 +1,4 @@
-include "lists.h"
+#include "lists.h"
 
 /**
  * get_nodeint_at_index -  function that returns the nth node of a listint_t linked list.

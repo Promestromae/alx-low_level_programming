2ce5691384cc729f90193d91fe820664f9bc9dcb
Promestromae <nzuvameshack@gmail.com>
2023-07-28 02:07:24 -0700

    promesmaestro

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * listint_len - Prints the number of elements in a linked listint_t list.
+ * listint_len - Prints the number of elements in a linked list.
  * @h: Linked list to be traversed.
  *
  * Return: Number of nodes.
  */
-size_t listint_len(const listint_t *h)
+size_t listint_len(const listint_t *h)	
 {
         size_t num = 0;
 
